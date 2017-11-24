909fb0da6a3621727eef1708881e3abb23c13d00
Don <sipher@nothing.com>
2017-11-24 09:21:10 +0530

    Project Created

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Trinetra on 11/24/2017.
+ */
+public class HelloWorld {
+
+    // Please try to keep this code clean
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
