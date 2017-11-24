657f7156ae46722411575ab6d5e75db118ec2504
Don <sipher@nothing.com>
2017-11-24 09:23:27 +0530

    Added Second Message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     // Please try to keep this code clean
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Jhello, World!");
     }
 }
